73f4724bef533ada4670217a00d18d1def834304
Diegooaraujo <149812280+Diegooaraujo@users.noreply.github.com>
2024-10-18 19:53:06 -0300

    herança

---
+++ heranca/heranca/src/entities/BussinesAccount.java
@@ -0,0 +1,29 @@
+package entities;
+
+public class BussinesAccount extends Account {
+    private Double loanLimit;
+
+    public BussinesAccount(){};
+
+    public BussinesAccount(Integer number, String titular, Double balance, Double loanLimit) {
+        super(number, titular, balance);
+        this.loanLimit = loanLimit;
+    }
+
+    public Double getLoanLimit() {
+        return loanLimit;
+    }
+
+    public void setLoanLimit(Double loanLimit) {
+        this.loanLimit = loanLimit;
+    };
+    
+    public void loan (double amount){
+        if(amount <= loanLimit){
+            saldo+=amount-10.0;;
+        }
+        
+
+    }
+    
+}
