aa9c45f4ea26b1cb7c942a489227be19580db410
Diegooaraujo <149812280+Diegooaraujo@users.noreply.github.com>
2024-10-19 15:27:11 -0300

    Classe-e-metodo-Final

---
+++ ClasseEmetodosFinal/src/App.java
@@ -0,0 +1,6 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+       //palavra chave "final" evita que a clsse seja herdada, tambem evita que o metodo seja sobreposto
+       
+    }
+}
